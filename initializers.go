fcfa054d11f6fea09ee3491cedac304a4e2350ba
tomás <67541868+tomasproanop@users.noreply.github.com>
2024-04-02 23:04:41 +0200

    Create initializers.go

---
+++ initializers.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+var i, j int = 1, 2
+var cat, dog string = "Ron", "Jay"
+
+func main() {
+	var c, python, java = true, false, "no!"
+	fmt.Println(i, j, c, python, java, cat, dog)
+}
