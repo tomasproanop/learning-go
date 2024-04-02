c3a8dae9c02c68958f75593bb937c59d4d975b58
tomás <67541868+tomasproanop@users.noreply.github.com>
2024-04-02 22:42:40 +0200

    Create swap.go

---
+++ swap.go
@@ -0,0 +1,20 @@
+#Multiple results
+
+package main
+
+import "fmt"
+
+func swap(x, y string) (string, string) {
+	return y, x
+}
+
+func swap2(c,d int) (int, int){
+return d, c}
+
+func main() {
+	a, b := swap("hello", "world")
+	fmt.Println(a, b)
+	
+	c, d := swap2(1, 10)
+	fmt.Println(c,d)
+}
