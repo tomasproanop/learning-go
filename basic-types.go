40ca3904eaa7e76f0232df9343bf107689dc3bfd
tomás <67541868+tomasproanop@users.noreply.github.com>
2024-04-03 13:57:16 +0200

    Update basic-types.go

--- basic-types.go
+++ basic-types.go
@@ -18,7 +18,9 @@ func main() {
 }
 
 
-#Output
-#Type: bool Value: false
-#Type: uint64 Value: 18446744073709551615
-#Type: complex128 Value: (2+3i)
+/*
+Output
+Type: bool Value: false
+Type: uint64 Value: 18446744073709551615
+Type: complex128 Value: (2+3i)
+*/
