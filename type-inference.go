26c02127e5ef9216bdb62720953337d6d29fec79
tomás <67541868+tomasproanop@users.noreply.github.com>
2024-04-03 14:00:31 +0200

    Update type-inference.go

--- type-inference.go
+++ type-inference.go
@@ -8,8 +8,10 @@ func main() {
 }
 
 
-#When the right hand side of the declaration is typed, 
-#the new variable is of that same type.
+/*
+"When the right hand side of the declaration is typed, 
+the new variable is of that same type.
 
-#But when the right hand side contains an untyped numeric constant, 
-# the new variable may be an int, float64, or complex128 depending on the precision of the constant.
+But when the right hand side contains an untyped numeric constant, 
+the new variable may be an int, float64, or complex128 depending on the precision of the constant."
+*/
