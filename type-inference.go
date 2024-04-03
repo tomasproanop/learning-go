778e8c039535e9637a555551298eb38d5983444f
tomás <67541868+tomasproanop@users.noreply.github.com>
2024-04-03 13:18:20 +0200

    Create type-inference.go

---
+++ type-inference.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	pi := 3.1415 
+	fmt.Printf("v is of type %T\n", v)
+}
+
+
+#When the right hand side of the declaration is typed, 
+#the new variable is of that same type.
+
+#But when the right hand side contains an untyped numeric constant, 
+# the new variable may be an int, float64, or complex128 depending on the precision of the constant.
